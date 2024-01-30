e86e7c7b4729e5048fe01aefee43583a31c2528f
LudoLaFayette <ludovicfaye89@gmail.com>
2024-01-30 15:00:07 +0100

    modification contenu mentions légales + modifcation nom

--- pages/mentions.vue
+++ pages/mentions.vue
@@ -1,4 +1,73 @@
 <template>
+    <h1> MENTIONS LÉGALES</h1>
+    <p>
+        En vigueur au 15/02/2024
+    </p>
+    <p>
+        Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la
+Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et
+visiteurs, ci-après l""Utilisateur", du site calendria.ludovicfaye.fr , ci-après le "Site", les présentes
+mentions légales.
+La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans
+réserve des présentes mentions légales.
+Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
+    </p>
+    <h2>
+        ARTICLE 1 - L'EDITEUR
+    </h2>
+<p>
+   
+
+
+
+L’édition et la direction de la publication du Site est assurée par Ludovic Faye, domiciliée 7 rue haute,
+89100 Nailly, dont le numéro de téléphone est 0789564952, et l'adresse e-mail
+ludovicfaye89@gmail.com.
+ci-après l'"Editeur".
+</p>
+<h2>ARTICLE 2 - L'HEBERGEUR</h2>
+<p>
+    L'hébergeur du Site est la société Infomaniak SA, dont le siège social est situé au 26, Avenue de la
+Praille – 1227 Carouge / Genève – SUISSE , avec le numéro de téléphone : 41228203544 +
+adresse mail de contact
+</p>
+<h2>
+    ARTICLE 3 - ACCES AU SITE
+</h2>
+
+<p>
+    Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption
+programmée ou non et pouvant découlant d’une nécessité de maintenance.
+En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.
+</p>
+<h2>
+    ARTICLE 4 - COLLECTE DES DONNEES
+</h2>
+
+<p>
+Le Site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect
+de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers
+et aux libertés.
+
+En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit
+d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur
+exerce ce droit :
+· par mail à l'adresse email ludovicfaye89@gmail.com
+· par voie postale au 7 rue haute, 89100 nailly ;
+· via un formulaire de contact ;
+· via son espace personnel ;
+Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site,
+sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires
+telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
+Pour plus d’informations, se reporter aux CGU du site calendria.ludovicfaye.fr accessible à la rubrique
+"CGU"
+Pour plus d'informations en matière de protection des données à caractère personnel , se reporter à
+la Charte en matière de protection des données à caractère personnel du site calendria.ludovicfaye.fr
+accessible à la rubrique "Données personnelles"
+Pour plus d'informations en matière de cookies, se reporter à la Charte en matière de cookies du site
+calendria.ludovicfaye.fr accessible à la rubrique "Cookies".
+
+</p>
 
 </template>
 <style scoped>
